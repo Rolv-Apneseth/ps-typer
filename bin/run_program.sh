b6ce3fa735fb4cac0c92c85f09cd019b5a41def7
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-04-22 18:31:15 +0100

    Made significant improvements to the bash script for installing dependencies and launching speed-typer

--- bin/run_program.sh
+++ bin/run_program.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# Author:
+#   Rolv Apneseth <rolv.apneseth@gmail.com>
+# 
+# Description:
+#   This is a script used to:
+#       1. Initialise a virtual environment for speed-typer using
+#          venv
+#       2. Install any dependencies speed-typer needs into this
+#          virtual environment
+#       3. Launch the program with the virtual enviroment
+#
+#   Note that environment and dependencies set up will only happen
+#   if certain files are not found so effectively it will only
+#   install dependencies once and won't try to do so on every 
+#   launch. 
+# ------------------------------------------------------------------
 
-# navigate our of bin directory
-cd ..
 
 # PATHS
-CONTAINING_FOLDER=$(pwd)
-ACTIVATE="$CONTAINING_FOLDER/bin/activate"
-PYTHON3="$CONTAINING_FOLDER/bin/python3"
+# This extremely useful one-liner was found here: https://stackoverflow.com/a/246128/14316282
+# Note that this will not work with symlinks
+BIN_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ACTIVATE="$BIN_FOLDER/activate"
+PYTHON3="$BIN_FOLDER/python3"
+PIP3="$BIN_FOLDER/pip3"
+
+CONTAINING_FOLDER="$(dirname "$BIN_FOLDER")"
+REQUIREMENTS="$CONTAINING_FOLDER/requirements.txt"
 PYQT5="$CONTAINING_FOLDER/lib/python3.8/site-packages/PyQt5"
 MAIN="$CONTAINING_FOLDER/speed-typer/main.py"
 
 # SETUP
 # Create venv if one does not exist. Also upgrades venv pip version
-# In either case, activate venv
 if [ ! -f "$PYTHON3" ]; then
-    cd ..
-    python3 -m venv speed-typer
-    cd "$CONTAINING_FOLDER"
+    python3 -m venv $CONTAINING_FOLDER
+    printf "\nVirtual environment created\n\n"
 
-    echo .
-    echo Virtual environment created
-    echo .
-
-    source "$ACTIVATE" && pip3 install --upgrade pip
-
-    echo .
-    echo Pip upgraded
-    echo .
-else
-    source "$ACTIVATE"
+    $PIP3 install --upgrade pip
+    printf "\nPip upgraded\n"
 fi
 
 # Install requirements if they are not already installed
 if [ ! -d "$PYQT5" ]; then
-    pip3 install -r requirements.txt
-
-    echo .
-    echo Dependencies installed
-    echo .
+    $PIP3 install -r $REQUIREMENTS
+    printf "\nDependencies installed\n"
 fi
 
 # RUN PROGRAM
-python3 "$MAIN"
+$PYTHON3 $MAIN
